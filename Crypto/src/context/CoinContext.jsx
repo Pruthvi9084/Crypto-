bba49b8852cfc9636ef19a964890f8e1f70a9c6c
Pruthvi@02 <128472563+Pruthvi9084@users.noreply.github.com>
2024-08-04 22:38:06 +0530

    Update CoinContext.jsx

--- Crypto/src/context/CoinContext.jsx
+++ Crypto/src/context/CoinContext.jsx
@@ -13,7 +13,7 @@ const CoinContextProvider = (props) =>{
     const fetchAllCoin = async () =>{
         const options = {
             method: 'GET',
-            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-3qwpVQEx1VnU7TYcqvTjv85Z'}
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-TA7vpEyeSxmFHjiuRc8JcrKM'}
           };
           
           fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`, options)
